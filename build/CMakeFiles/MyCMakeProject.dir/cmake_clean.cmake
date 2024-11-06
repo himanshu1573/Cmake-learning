4440bedaecd7f5c75c0cf6ce98cfa66d67337e2a
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2024-11-06 18:12:15 +0530

    adding files

---
+++ build/CMakeFiles/MyCMakeProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCMakeProject.dir/main.cpp.o"
+  "CMakeFiles/MyCMakeProject.dir/main.cpp.o.d"
+  "MyCMakeProject"
+  "MyCMakeProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCMakeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
